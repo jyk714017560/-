64b5fac3dcdac85e7268b19036d8b0003543ca94
JYK <714017560@qq.com>
2018-04-03 22:30:26 +0800

    4/3-1

--- Scripts/NewBehaviourScript.cs
+++ Scripts/NewBehaviourScript.cs
@@ -5,20 +5,51 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour {
 
 	public float MoveSpeed;
+	public GameObject Food;
 
 	// Use this for initialization
 	void Start () {
-		
+		Creat_Food ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.Translate(Vector3.forward*Time.deltaTime*MoveSpeed)
+		transform.Translate (Vector3.forward * Time.deltaTime * MoveSpeed);
 
-		if(Input.GetButton(KeyCode.UpArrow))
+		if(Input.GetKeyDown(KeyCode.UpArrow)&&Vector3.Angle(Vector3.forward,this.transform.forward)<=110f)
 		{
-			transform.
+			transform.forward = Vector3.forward;
 		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)&&Vector3.Angle(Vector3.back,this.transform.forward)<=110f)
+		{
+			this.transform.forward = Vector3.back;
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow)&&Vector3.Angle(Vector3.left,this.transform.forward)<=110f)
+		{
+			this.transform.forward = Vector3.left;
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow)&&Vector3.Angle(Vector3.right,this.transform.forward)<=110f)
+		{
+			this.transform.forward = Vector3.right;
+		}
+			
+	}
+
+	void Creat_Food()
+	{
+		float Food_x=Random.Range(-4f,4f);
+		float Food_z=Random.Range(-4f,4f);
+		Food = Instantiate (Food, new Vector3 (Food_x, 0, Food_z),Quaternion.identity)as GameObject;
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{  
+		if (other.tag=="food") 
+		{  			
+			Destroy (other.gameObject);  
+			Creat_Food ();
+		}  
 	}
 }
